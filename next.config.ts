944069c663eca7ef15d2e1e9b616b6325f2e527f
Tikaram Acharya <tikaram19ac@gmail.com>
2026-01-14 10:09:45 +0545

    Fix basePath for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // output: "export",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // No basePath needed for custom domain (tikaram-acharya.com.np)
 };
 
 export default nextConfig;
